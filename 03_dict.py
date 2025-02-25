9630f1ac0bc22c4b1c085015f284212b0318730c
albecabrera <albe.cabrera@yahoo.de>
2025-02-25 13:36:44 +0100

    second commit

---
+++ 03_dict.py
@@ -0,0 +1,12 @@
+camiseta = {
+"color" : "rojo",
+"talla" : "L",
+"precio" : 100,
+"unidades" : 10
+}
+ # Acceso a los elementos del diccionario
+print(camiseta["color"])
+print(camiseta["talla"])
+print(camiseta["precio"])
+print(camiseta["unidades"])
+
