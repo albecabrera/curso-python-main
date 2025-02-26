cb3cb513ea5deb81f8fe5ce6458b5f082bde7901
albecabrera <albe.cabrera@yahoo.de>
2025-02-26 11:02:26 +0100

    otro cambio actualizado

---
+++ 04_practica.py
@@ -0,0 +1,29 @@
+'''
+Ahora voy a practicar
+código en Python para 
+seguir aprendiendo
+'''
+# Declarar variables
+name = "Pedro"
+age = 20
+city = "Bonn"
+job = "estudiante"
+print(name)
+print(age)
+print(city)
+print(job)
+
+print(f"Su nombre es {name} y vive en {city}. Además su oficio es {job} ")
+print(f"Su edad es: {age} ")
+
+# Sumar números
+numero1 = 80
+numero2 = 30
+suma_numeros = numero1 + numero2
+resta_numeros = numero1 - numero2
+division_numeros = numero1 / numero2
+print(f"La suma de los numeros es igual a", suma_numeros)
+print(f"La resta de los numeros es igual a", resta_numeros)
+print(f"La división de los numeros es igual a", division_numeros)
+
+
